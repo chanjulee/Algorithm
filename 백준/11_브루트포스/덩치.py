b50e6102f7520caf2b25d19049da3b80603587d4
DESKTOP-1DSGHO9\JOO <dlckswn52@gmail.com>
2021-01-03 14:11:13 +0900

    알고리즘
    브루트포스

--- 백준/11_브루트포스/덩치.py
+++ 백준/11_브루트포스/덩치.py
@@ -1,4 +1,25 @@
 import sys
+if __name__ == "__main__":
+    N = int(sys.stdin.readline())
+    chart = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
+    for c in chart:
+        temp = 1
+        for d in chart:
+            if c[0]<d[0] and c[1]<d[1]:
+                temp += 1
+        print(temp,end=' ')
+
+
+
+
+
+
+
+
+
+
+'''
+import sys
 sys.stdin=open("input.txt","rt")
 N = int(sys.stdin.readline().strip())
 men = []
@@ -11,3 +32,4 @@ for i in men:
         if i[0] < j[0] and i[1] < j[1]:
             rank += 1
     print(rank, end=' ')
+'''
