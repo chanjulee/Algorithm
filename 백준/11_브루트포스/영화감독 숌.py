80682469f7557c33ddac19199285d5b261be142e
DESKTOP-1DSGHO9\JOO <dlckswn52@gmail.com>
2021-01-04 15:42:15 +0900

    알고리즘
    브루트포스

--- 백준/11_브루트포스/영화감독 숌.py
+++ 백준/11_브루트포스/영화감독 숌.py
@@ -1,3 +1,33 @@
+import sys
+if __name__ == "__main__":
+    N = int(sys.stdin.readline())
+    num = 0
+    count = 0
+    while True:
+        num += 1
+        if '666' in str(num):
+            count += 1
+        if count == N:
+            break
+    print(num)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 N = 7
 
 title = 666 #제일 처음 타이틀
@@ -6,3 +36,4 @@ while N:
         N-=1 #666이 들어갈때마다 N줄여줌
     title+=1 #1씩 늘려감
 print(title-1) #마지막에 1더한거 빼주고 출력
+'''
