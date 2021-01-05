6b52955bb8324737ef45b78ee1bb952e7594caf5
DESKTOP-1DSGHO9\JOO <dlckswn52@gmail.com>
2021-01-05 14:14:10 +0900

    알고리즘
    브루트포스

--- 백준/11_브루트포스/체스판 다시 칠하기.py
+++ 백준/11_브루트포스/체스판 다시 칠하기.py
@@ -1,11 +1,38 @@
+def countWB(tempChess):
+    cnt1 = 0 #처음칸 W
+    for i in range(8):
+        for j in range(8):
+            if i%2==0 and j%2==0: #W여야함
+                if tempChess[i][j]!='W':
+                    cnt1 += 1
+            elif i%2==1 and j%2==1: #W여야함
+                if tempChess[i][j]!='W':
+                    cnt1 += 1
+            else: #B여야함
+                if tempChess[i][j]!='B':
+                    cnt1 += 1
+    cnt2 = 0 #처음칸 B
+    for i in range(8):
+        for j in range(8):
+            if i%2==0 and j%2==0: #B여야함
+                if tempChess[i][j]!='B':
+                    cnt2 += 1
+            elif i%2==1 and j%2==1: #B여야함
+                if tempChess[i][j]!='B':
+                    cnt2 += 1
+            else: #W여야함
+                if tempChess[i][j]!='W':
+                    cnt2 += 1
+    #print("cnt1:", cnt1, ",cnt2:", cnt2)
+    return min(cnt1,cnt2)
+
 import sys
-sys.stdin=open("input.txt","rt")
-N, M = map(int, input().split())
-Chess = [input() for i in range(N)]
-
-for i in range(8):
-    for j in range(8):
-        #흰칸으로 시작하는 경우
-        if 
-        #검은칸으로 시작하는 경우
-
+if __name__ == "__main__":
+    N, M = map(int,sys.stdin.readline().split())
+    Chess = [list(sys.stdin.readline().rstrip()) for _ in range(N)]
+    count = []
+    for i in range(N-7):
+        for j in range(M-7):
+            tempChess = [temp[j:(8+j)] for temp in Chess[i:(8+i)]] #참고
+            count.append(countWB(tempChess))
+    print(min(count))
